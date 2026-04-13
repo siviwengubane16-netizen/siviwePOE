3399dabc5069674053f10d8d3c3563ebe1db9d88
siviwengubane16-netizen <siviwengubane16@gmail.com>
2026-04-13 17:39:39 +0200

    Add files via upload

---
+++ user.java
@@ -0,0 +1,29 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package siviwepoe;
+
+/**
+ *
+ * @author Student
+ */
+public class user {
+    private String username;
+    private String password;
+    private String cellphone;
+
+    public user(String username, String password, String cellphone) {
+        this.username = username;
+        this.password = password;
+        this.cellphone = cellphone;
+    }
+
+    public String getUsername() { return username; }
+    public String getPassword() { return password; }
+    public String getCellphone() { return cellphone; }
+
+    
+}
+    
+
